b76f7222fd8999c88692d7f82106fa9e032e1f25
Naithss <109505490+Naithss@users.noreply.github.com>
2023-03-07 02:19:53 -0800

    Update Bronas Loadstring.lua

--- Bronas Loadstring.lua
+++ Bronas Loadstring.lua
@@ -5,5 +5,6 @@
 ██╔══██╗██╔══██╗██║░░██║██║╚████║██╔══██║░╚═══██╗
 ██████╦╝██║░░██║╚█████╔╝██║░╚███║██║░░██║██████╔╝
 ╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░
+This script is only for Broken Bones IV
 ]]
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Naithss/Roblox/main/Bronas.lua')))()
